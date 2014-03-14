bb896da9cd5d35feff6c4711e6f28eba4ef061d1
David Gamba <david.gamba@calgaryscientific.com>
2014-03-14 11:08:09 -0600

    Remove welcome screen

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
+; No welcome screen
+(setq inhibit-splash-screen t)
+(setq inhibit-startup-message t)
+
 ; Add evil repository path
 ; git clone git://gitorious.org/evil/evil.git
 (add-to-list 'load-path "~/.emacs.d/evil")
