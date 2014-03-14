8ac953abc118e3a93c1e7113c21731c206f661d9
David Gamba <david.gamba@calgaryscientific.com>
2014-03-14 11:09:58 -0600

    Add line numbers

--- init.el
+++ init.el
@@ -2,6 +2,11 @@
 (setq inhibit-splash-screen t)
 (setq inhibit-startup-message t)
 
+; line numbers
+(global-linum-mode t)
+; line number format 4 digits and | symbol
+(setq linum-format "%4d \u2502 ")
+
 ; Add evil repository path
 ; git clone git://gitorious.org/evil/evil.git
 (add-to-list 'load-path "~/.emacs.d/evil")
