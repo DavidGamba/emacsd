2bc179013fd97f2801399dfe3a18b17efc9d47bc
David Gamba <david.gamba@calgaryscientific.com>
2014-03-16 14:01:42 -0600

    Add keybindings to move around splits and frames

--- config/init-keybindings.el
+++ config/init-keybindings.el
@@ -6,4 +6,15 @@
   "," 'evilnc-comment-or-uncomment-lines
 )
 
+; More around splits
+(define-key evil-normal-state-map (kbd "C-h") 'evil-window-left)
+(define-key evil-normal-state-map (kbd "C-j") 'evil-window-down)
+(define-key evil-normal-state-map (kbd "C-k") 'evil-window-up)
+(define-key evil-normal-state-map (kbd "C-l") 'evil-window-right)
+
+; Create new frames and switch between them
+(evil-ex-define-cmd "fsp" 'new-frame)
+(define-key evil-normal-state-map (kbd "C-S-h") 'other-frame)
+(define-key evil-normal-state-map (kbd "C-S-l") 'other-frame)
+
 (provide 'init-keybindings)
